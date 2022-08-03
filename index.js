ae22c6083cfc36117cd0203275d2402b40955ac3
rikiarizona <rikiarizona@gmail.com>
2022-08-03 10:30:01 +0700

    TUGAS 26 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,25 @@
+function periksaInf() {
+    var a = isFinite([2])
+    if (a === true) console.log("Angka 2 NOT Infinity.");
+    var b = isFinite([39])
+    if (b === true) console.log("Angka 39 NOT Infinity.");
+    var dua = isFinite([76])
+    if (dua === true) console.log("Angka 76 NOT Infinity.");
+    var dua = isFinite([50])
+    if (dua === true) console.log("Angka 50 NOT Infinity.");
+    var dua = isFinite([9])
+    if (dua === true) console.log("Angka 9 NOT Infinity.");
+    var dua = isFinite([7])
+    if (dua === true) console.log("Angka 7 NOT Infinity.");
+    var dua = isFinite([41])
+    if (dua === true) console.log("Angka 41 NOT Infinity.");
+    var dua = isFinite([2])
+    if (dua === true) console.log("Angka 2 NOT Infinity.");
+    var dua = isFinite([24])
+    if (dua === true) console.log("Angka 24 NOT Infinity.");
+    var dua = isFinite([1])
+    if (dua === true) console.log("Angka 1 NOT Infinity.");
+    var dua = isFinite([16])
+    if (dua === true) console.log("Angka 16 NOT Infinity.");
+}
+periksaInf();
